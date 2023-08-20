96417dc5217fa7b302848e231a37d6930fea596b
Selasi3 <selasiayittah3@gmail.com>
2023-08-20 09:58:12 +0000

    modified files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1c3d07cdd63dd21b"; }
+/******/ 		__webpack_require__.h = function() { return "e87056f8bdbb5bac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
